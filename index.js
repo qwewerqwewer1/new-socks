491734bd399874c841e099fffa417efa7eed6cae
Dima Kovyazin <kovyazinaa11@yandex.ru>
2022-07-15 14:48:50 +0300

    add html, add js, add css

---
+++ index.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Носочки',
+    description: 'Пара теплых пушистых носков',
+    image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+    altText: 'Пара носков',
+    inStock: false,
+  }
+})
