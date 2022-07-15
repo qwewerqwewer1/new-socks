5fe0340c7004fe0f95cf0c02d02652062235720b
Dima Kovyazin <kovyazinaa11@yandex.ru>
2022-07-15 17:10:40 +0300

    add v-bind:key variant

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Носочки',
+    product: 'Socks',
     description: 'Пара теплых пушистых носков',
     image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
