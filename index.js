450f16cd8d2adbf0a5c4f71a32932214c49275f4
Dima Kovyazin <kovyazinaa11@yandex.ru>
2022-07-15 15:47:57 +0300

    add v-for

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ var app = new Vue({
     image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
     altText: 'Пара носков',
-    inStock: false,
+    inStock: 11,
+    onSale: true,
+    details: ['80% хлопок', '20 полиэстер', 'одинаковые'],
+    variants: [
+      {
+        variantId: 124,
+        variantColor: 'green',
+      }, 
+      {
+        variantId: 125,
+        variantColor: 'blue',
+      }
+    ]
   }
 })
