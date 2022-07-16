03b30b6e2d72e4c2389a1c417a4e423d80eeefbc
dima <kovyazinaa11@yandex.ru>
2022-07-16 14:50:53 +0300

    add new btn

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Socks',
-    description: 'Пара теплых пушистых носков',
-    image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+    description: 'Пара теплых пушистых носков',
     altText: 'Пара носков',
-    inStock: false,
-    onSale: true,
     details: ['80% хлопок', '20 полиэстер', 'одинаковые'],
     sizes: [34,37,40],
+    product: 'Socks',
+    selectedVariant: 0,
+    onSale: true,
     variants: [
       {
         variantId: 124,
         variantColor: 'green',
-        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        variantQuantity: 9,
       },
       {
         variantId: 125,
         variantColor: 'blue',
-        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+        variantQuantity: 0,
       }
     ],
     cart: 0,
@@ -31,13 +32,16 @@ var app = new Vue({
     removeFromCart() {
       this.cart--
     },
-    updateImage(newImage) {
-      this.image = newImage
+    updateProduct(i) {
+      this.selectedVariant = i
     },
-    
-    
   },
   computed: {
-    
+   image() {
+    return this.variants[this.selectedVariant].variantImage
+   },
+   inStock() {
+    return this.variants[this.selectedVariant].variantQuantity
+   }
   }
 })
