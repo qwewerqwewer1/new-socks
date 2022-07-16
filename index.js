abea8eff41942cdf816284a1862336df8bd90078
dima <kovyazinaa11@yandex.ru>
2022-07-16 13:58:29 +0300

    add new btn

--- index.js
+++ index.js
@@ -6,18 +6,38 @@ var app = new Vue({
     image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
     altText: 'Пара носков',
-    inStock: 11,
+    inStock: false,
     onSale: true,
     details: ['80% хлопок', '20 полиэстер', 'одинаковые'],
+    sizes: [34,37,40],
     variants: [
       {
         variantId: 124,
         variantColor: 'green',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
       },
       {
         variantId: 125,
         variantColor: 'blue',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
       }
-    ]
+    ],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart++
+    },
+    removeFromCart() {
+      this.cart--
+    },
+    updateImage(newImage) {
+      this.image = newImage
+    },
+    
+    
+  },
+  computed: {
+    
   }
 })
